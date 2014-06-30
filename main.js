eee6dcc1a5307b742e3a9d9da6646c00b22f5c40
gbachik <gbachik@gmail.com>
2014-06-30 11:07:55 -0600

    Finished Project

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-$(document).on('ready', function() {
-  
-});
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+function one(){
+	for(var i = 0; i < animals.length-1; i++){
+		console.log(animals[i]);
+	}
+}
+
+function two(){
+	for(var i = 1; i < animals.length; i++){
+		console.log(animals[i]);
+	}
+}
+
+function three(){
+	for(var i = animals.length; i > 0; i--){
+		console.log(animals[i]);
+	}
+}
+
+function four(){
+	for(var i = 0; i < animals.length; i++){
+		if (animals[0] || animals[animals.length]) {
+			console.log(animals[i]);
+		}else{
+			console.log(animals[i]);
+			console.log(animals[i]);
+		}
+	}
+}
